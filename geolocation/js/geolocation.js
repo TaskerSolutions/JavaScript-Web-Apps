d07b0f48c1590d84adf86b35d9c3a5a2f18a836f
harry <harrytasker999@gmail.com>
2022-04-03 15:07:06 +1200

    geolocation test

--- geolocation/js/geolocation.js
+++ geolocation/js/geolocation.js
@@ -1,5 +1,6 @@
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
+const text = document.querySelector('.text-output');
 
 // getCurrentPosition() can be used to pull position once.
 // watchPosition() will continue to pull over time.
@@ -12,7 +13,11 @@ navigator.geolocation.watchPosition((data) => {
   arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 }, 
 // if user does not grant access to geolocation data
-(error) => {
-  console.err(error);
+(err) => {
+  console.err(err);
   alert('You must allow access to geolocation data for this app to work.')
+});
+
+navigator.geolocation.watchPosition((data) => {
+  text.textContent = "speed: " + data.coords.speed + "degrees: " + data.coords.heading + " lattitude: " + data.coords.latitude;
 });
